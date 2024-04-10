d7503e994e8f2b16a9ebeffffd0d68cdf321c1a6
asfahl <davidgbaier@gmx.de>
2024-04-10 15:30:28 +0200

    Exercise tensor generation finished

--- 2_1_Tensors_Python/Tensor_generation.py
+++ 2_1_Tensors_Python/Tensor_generation.py
@@ -29,3 +29,23 @@ start = timeit.timeit()
 t6 = torch.from_numpy(arr)
 end = timeit.timeit()
 print(f"Laufzeit für Tensor aus NP Array:{end-start}")
+
+
+# Tensor from list of lists
+T = torch.empty(4,2,3)
+T0 = [[0, 1, 2], [3, 4, 5]]
+T1 = [[6, 7, 8], [9, 10, 11]]
+T2 = [[12, 13, 14], [15, 16, 17]]
+T3 = [[18, 19, 20], [21, 22, 23]]
+l = [T0, T1, T2, T3]
+T = torch.tensor(l)
+print(T)
+print(T.shape)
+
+# Tensor from NP Array
+arr = np.array(l)
+T = torch.tensor(arr)
+print(arr)
+print(T)
+print(T.shape)
+
