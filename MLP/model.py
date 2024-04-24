6537dcb0967997969648d41895222b727eda41ba
asfahl <davidgbaier@gmx.de>
2024-04-24 13:08:06 +0200

    initiate Model

--- MLP/model.py
+++ MLP/model.py
@@ -5,4 +5,8 @@ from torch.utils.data import DataLoader
 class MLP(nn.Module):
     def __init__(self):
         super().__init__()
-        
+        # flattens two dimensional image tensor into one dimensional tensor
+        self.flatten = nn.Flatten()
+        # 28x28 = 784
+        self.linear = nn.Linear(28*28, 1000)
+
