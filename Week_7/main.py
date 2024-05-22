e15b5a506a81fe70a752ed7ce399619095fc111c
asfahl <davidgbaier@gmx.de>
2024-05-22 13:14:43 +0200

    add 10.1.1

--- Week_7/main.py
+++ Week_7/main.py
@@ -1,1 +1,10 @@
 # conda activate /mnt/hd1/conda/aimet
+
+# 10.1.1
+# compute_encodings
+# Berechnet die optimale Quantisierung der Gewicht und Aktivierungsfkt. eines trainierten
+# Models. Ein representativer Datensatz ist zusätzlich erforderlich.
+#
+# forward_pass_callback
+# Der Parameter von compute_encodings ist eine Callback-Fkt. die die Forward-Fkt. des Modells auf einem gegebenen Datansatz aufruft.
+# Auf Basis des durchlaufs der Forward Funktion errechnet compute_emcodings dann die Quantisierung.  
