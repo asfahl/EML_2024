08d02cc86dac6a2749408702ec4e59fd0ef665ec
Asfahl <davidgbaier@gmx.de>
2024-06-26 21:32:57 +0200

    finally working

--- Week_12/main.py
+++ Week_12/main.py
@@ -1,24 +1,21 @@
 import os
 import torch
-import torch.nn as nn
 import torch.distributed as dist
 import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel
 
+# Adaptet from Pytorch documentation
 def run(rank, size):
     print(f"Rank: {rank}")
     print(f"Size: {size}")
     return
 
-# adapted from ollama Mistral
-def init_process(rank, size, fn):
-    os.environ["MASTER_ADDR"] = "localhost"  # Set the master address to localhost
-    os.environ["MASTER_PORT"] = "0"
-    os.environ["WORLD_SIZE"] = ""+size
-    #os.environ["WORLD_SIZE"] = str(torch.cuda.device_count())  # Get the number of GPUs in the Grace system
-    torch.cuda.device(rank)  # Set the device to the current GPU (for this rank)
-    dist.init_process_group(backend="gloo", rank=rank, world_size= int(os.environ["WORLD_SIZE"]))  # Initialize the MPI backend for distributed communication
-    fn(rank, os.environ["WORLD_SIZE"])
+
+def init_process(rank, size, fn, backend='MPI'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '0'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
 
 
 if __name__ == "__main__":
@@ -26,9 +23,18 @@ if __name__ == "__main__":
     processes = []
     mp.set_start_method("spawn")
     for rank in range(size):
-        p = mp.Process(target=init_process, args=[rank, size, run])
+        p = mp.Process(target=init_process, args=(rank, size, run))
         p.start()
         processes.append(p)
-    
+
     for p in processes:
         p.join()
+### Output
+#work/EML/pytorch/torch/distributed/distributed_c10d.py:1480: UserWarning: For MPI backend, world_size (2) and rank (0) are ignored since they are assigned by the MPI runtime.
+#  warnings.warn(
+#/work/EML/pytorch/torch/distributed/distributed_c10d.py:1480: UserWarning: For MPI backend, world_size (2) and rank (1) are ignored since they are assigned by the MPI runtime.
+#  warnings.warn(
+#Rank: 1
+#Size: 2
+#Rank: 0
+#Size: 2
